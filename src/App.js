ca424914ae25ca4241c8f1f1242bee70342f9903
Nicklye26 <nicklye26@gmail.com>
2023-02-05 18:14:15 +0900

    installed react-router and implemented, added CustomLink function to have an active display with a nav link is clicked(active)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import "./App.css";
 import Navbar from "./components/NavBar";
 import Landing from "./pages/Landing";
+import Profile from "./pages/Profile";
+import Forum from "./pages/Forum";
+import Messenger from "./pages/Messenger";
+import Contact from "./pages/Contact";
+import Map from "./pages/Map";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
+    <>
       <Navbar />
-      <Landing />
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/forum" element={<Forum />} />
+          <Route path="/messenger" element={<Messenger />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/map" element={<Map />} />
+        </Routes>
+      </div>
+    </>
   );
 };
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,39 @@
 import React from "react";
 import "./Navbar.css";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="nav">
-      <a href="/" className="site-title">
+      <Link to="/" className="site-title">
         Site Name
-      </a>
+      </Link>
       <ul>
-        <li>
-          <a href="/profile">Profile</a>
-        </li>
-        <li>
-          <a href="forum">Forum</a>
-        </li>
-        <li>
-          <a href="messenger">Messenger</a>
-        </li>
-        <li>
-          <a href="contact">Contact</a>
-        </li>
-        <li>
-          <a href="map">Map</a>
-        </li>
+        <CustomLink to="/">Home</CustomLink>
+        <CustomLink to="/profile">Pofile</CustomLink>
+        <CustomLink to="/forum">Forum</CustomLink>
+        <CustomLink to="/messenger">Messenger</CustomLink>
+        <CustomLink to="/contact">Contact</CustomLink>
+        <CustomLink to="/map">Map</CustomLink>
+        {/*Logout button not working yet*/}
+        <li>Logout</li>
       </ul>
     </nav>
+  );
+}
+
+// to have an active display when CustomLink is selected
+function CustomLink({ to, children, ...props }) {
+  // console.log(window.location.pathname);
+  // const path = window.location.pathname;
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname });
+
+  return (
+    <li className={isActive === to ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
   );
 }
