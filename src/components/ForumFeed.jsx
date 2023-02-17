7d986dade807bf703b2fdd16f6bd3788016c6d19
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2023-02-18 01:40:11 +0800

    Done Forum page, partially done Forum Individual Feed

--- src/components/ForumFeed.jsx
+++ src/components/ForumFeed.jsx
@@ -1,45 +1,72 @@
 import React, { useState, useEffect, useContext } from "react";
 import { ForumContext } from "../contexts/ForumContext";
 import { UserContext } from "../contexts/UserContext";
+import { Link } from "react-router-dom";
+import { BsArrowDownSquare, BsArrowUpSquare } from "react-icons/bs";
 
 const ForumFeed = () => {
   const allForumData = useContext(ForumContext);
   const { userData, allUserData } = useContext(UserContext);
-  const allStudentData = allUserData.filter((user) => user.student)
+  const allStudentData = allUserData.filter((user) => user.student);
 
-  console.log(allForumData);
-  console.log(allStudentData);
+  // console.log(allForumData);
+  // console.log(allStudentData);
 
   return (
     <div className="h-full rounded-lg">
       <div className="overflow-auto p-1 h-[550px]">
         {allForumData &&
           allForumData.map((forum, index) => (
-            <div className="bg-white rounded-lg mt-4 h-48 pt-4 pl-5 grid grid-cols-1 gap-5">
-              <div className="bg-darkgrey rounded-lg text-yellow text-sm font-bold w-[600px] py-1 indent-4">
-                {forum.course.course_code} - {forum.course.course_name}
-              </div>
-              <div className="grid grid-flow-col grid-cols-8 justify-start">
-                <div className="text-darkgrey">Post Upvote</div>
-                <div className="text-darkgrey text-lg col-span-7 font-extrabold">
-                  <p>Latest Post:</p>
-                  <p className="text-sm">
-                    "{forum.posts[forum.posts.length - 1].content}"
-                  </p>
-                  <p className="text-sm font-normal">by </p>
-                  <p className="text-sm font-normal">
-                    {allUserData && 
-                      allStudentData.filter(
-                        (user) =>
-                          user.student.id ===
-                          forum.posts[forum.posts.length - 1].studentId,
-                      )[0].first_name}
-                  </p>
+            <div
+              className="bg-white rounded-lg mt-4 h-48 pt-4 pl-5 grid grid-cols-1 gap-5"
+              key={forum.id}
+            >
+              <Link to={`/forum/${forum.id}`}>
+                <div className="bg-darkgrey rounded-lg text-yellow text-sm font-bold w-[600px] py-1 indent-4">
+                  {forum.course.course_code} - {forum.course.course_name}
                 </div>
-              </div>
-              <div className="text-darkgrey text-[10px] font-bold">
-                Created on {forum.updatedAt}
-              </div>
+                <div className="grid grid-flow-col grid-cols-8 justify-start mt-2">
+                  <div className="text-darkgrey grid grid-cols-1 font-extrabold text-xl">
+                    <BsArrowUpSquare className="hover:text-yellow hover:bg-darkgrey" />
+                    <span className="text-sm">
+                      100 <br /> Upvotes
+                    </span>
+                    <BsArrowDownSquare className="hover:text-yellow hover:bg-darkgrey" />
+                  </div>
+                  <div className="text-darkgrey text-lg col-span-7 font-extrabold">
+                    <p>Latest Post:</p>
+                    <p className="text-sm">
+                      "{forum.posts[forum.posts.length - 1].content}"
+                    </p>
+                    <p className="text-sm font-normal">by </p>
+                    <p className="text-sm font-normal">
+                      {allUserData &&
+                        `${
+                          allStudentData.filter(
+                            (user) =>
+                              user.student.id ===
+                              forum.posts[forum.posts.length - 1].studentId,
+                          )[0].first_name
+                        } ${
+                          allStudentData.filter(
+                            (user) =>
+                              user.student.id ===
+                              forum.posts[forum.posts.length - 1].studentId,
+                          )[0].last_name
+                        } on ${
+                          allStudentData.filter(
+                            (user) =>
+                              user.student.id ===
+                              forum.posts[forum.posts.length - 1].studentId,
+                          )[0].createdAt
+                        }`}
+                    </p>
+                  </div>
+                </div>
+                <div className="text-darkgrey text-[10px] font-bold">
+                  Created on {forum.updatedAt}
+                </div>
+              </Link>
             </div>
           ))}
       </div>
--- src/components/ForumFeedIndividual.jsx
+++ src/components/ForumFeedIndividual.jsx
@@ -1,13 +1,96 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from "react";
+import { ForumContext } from "../contexts/ForumContext";
+import { UserContext } from "../contexts/UserContext";
+import { useNavigate, useParams } from "react-router-dom";
+import { BsArrowDownSquare, BsArrowUpSquare } from "react-icons/bs";
 
 const ForumFeedIndividual = () => {
+  const param = useParams();
+
+  const allForumData = useContext(ForumContext);
+  const { userData, allUserData } = useContext(UserContext);
+  const allStudentData = allUserData.filter((user) => user.student);
+  const forum = allForumData.filter((forum) => forum.id === param.id)[0];
+
+  // console.log(allForumData);
+  // console.log(forum);
+  // console.log(allStudentData);
+
   return (
     <div className="h-full rounded-lg">
-    <div className="font-extrabold text-2xl text-yellow">
-      Student Forum Individual
-    </div>
+      <div className="p-1 h-[550px]">
+        {allForumData && (
+          <div className="bg-white rounded-lg mt-2 pt-2 pl-5 grid grid-cols-1 h-full content-start">
+            <div className="bg-darkgrey rounded-lg text-yellow text-sm font-bold w-[600px] h-fit py-1 indent-4 ">
+              {forum.course.course_code} - {forum.course.course_name}
+            </div>
+            <div className="grid grid-flow-col grid-cols-8 justify-start mt-2">
+              <div className="text-darkgrey grid grid-cols-1 font-extrabold text-xl h-24">
+                <BsArrowUpSquare className="hover:text-yellow hover:bg-darkgrey" />
+                <span className="text-sm">
+                  100 <br /> Upvotes
+                </span>
+                <BsArrowDownSquare className="hover:text-yellow hover:bg-darkgrey" />
+              </div>
+              <div className="text-darkgrey text-lg col-span-7 font-extrabold">
+                <div className="text-darkgrey text-[10px] font-bold">
+                  Created on {forum.updatedAt}
+                </div>
+                <div className="overflow-auto h-3/4 border-darkgrey border-1 mr-4 rounded-xl p-4 bg-slate-100">
+                  {allForumData &&
+                    forum.posts.map((post) => (
+                      <div className="grid grid-flow-col grid-cols-8 justify-start h-24">
+                        <img
+                          src={
+                            allStudentData.filter(
+                              (user) => user.student.id === post.studentId,
+                            )[0].profile_pic_url
+                          }
+                          alt="Forum Profile Pic"
+                          className="rounded-full w-12 h-12"
+                        />
+                        <div className="col-span-7 text-sm">
+                          <p>
+                            {
+                              allStudentData.filter(
+                                (user) => user.student.id === post.studentId,
+                              )[0].last_name
+                            }{" "}
+                            {
+                              allStudentData.filter(
+                                (user) => user.student.id === post.studentId,
+                              )[0].first_name
+                            }{" "}
+                            <span className="text-xs pl-6">
+                              - Updated at{" "}
+                              {
+                                allStudentData.filter(
+                                  (user) => user.student.id === post.studentId,
+                                )[0].updatedAt
+                              }
+                            </span>
+                          </p>
+                          <p className="font-normal text-xs mt-1">
+                            {post.content}
+                          </p>
+                          <p className="mt-2 flex flex-row justify-start gap-2 text-xs">
+                            <BsArrowUpSquare className="hover:text-yellow hover:bg-darkgrey" />
+                            <div>100</div>
+                            <BsArrowDownSquare className="hover:text-yellow hover:bg-darkgrey" />
+                            <div className="ml-5">Direct Message</div>
+                            <div className="ml-5">Report</div>
+                          </p>
+                        </div>
+                      </div>
+                    ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
 
-export default ForumFeedIndividual
+export default ForumFeedIndividual;
