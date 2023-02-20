e25a2f9797d226ca1c238dec67ed8028d7c8deb5
LoyChaiEe <celoy001@gmail.com>
2023-02-20 14:57:50 +0800

    Revert "added swr to comp"
    This reverts commit 1b8418fdad5bcd65f94f6dbd1a54b00ea5843296.

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,38 +4,28 @@ import Timetable from '../components/Timetable';
 import CourseReg from "../components/CourseReg";
 import "../App.css";
 import axios from 'axios';
-import useSWR from "swr";
-
-//must use the word 'use' to use hooks
-const useCourse = (course_code) =>{
-const {
-  data: course,
-  error,
-  isLoading,
-  mutate: refetch,
-} = useSWR(`http://localhost:3000/courses/${course_code}`, fetcher);
-
-return {course, error,isLoading, refetch}
-}
-
-const fetcher = (url) => axios.get(url).then((res) => res.data);
+// worry next time, need to relate to user
 const Landing = () => {
-  //const [course, setCourse] = useState([]);
-  const [ chosenIndex, setChosenIndex ] = useState(0); //courseIndex is an array of dictionary with the key being the courses and the value being the inde
-  const { course, error, isLoading, refetch } = useCourse("IE1005")
+  const [course, setCourse] = useState([]);
+  const [courseIndex, setCourseIndex] = useState([]); //courseIndex is an array of dictionary with the key being the courses and the value being the index
 
-  if(isLoading || error){ //need to put this down as the initial render will not render the data as we waiting response so undefined
-    return <div></div>
-  }
-  console.log(chosenIndex)
+  //call once when initialise
+  useEffect(() => {
+    axios.get("http://localhost:3000/courses/IE1005").then((res) =>{
+      setCourseIndex(res.data.course_indices);
+      console.log(courseIndex);
+    })
+  }, [])
+
+
   return (
     <div>
       <Logo />
       <div className="columns">
-        <Timetable course={course} chosenIndex={chosenIndex} />
+        <Timetable course={course} courseIndex={courseIndex} />
         <div className="rows">
-          <CourseReg course={course} setChosenIndex={setChosenIndex} />
-          <CourseReg course={course} setChosenIndex={setChosenIndex} />
+          <CourseReg course={course} courseIndex={courseIndex} />
+          <CourseReg course={course} courseIndex={courseIndex} />
         </div>
       </div>
     </div>
