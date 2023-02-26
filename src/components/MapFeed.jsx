7b5e284c372495633082ff9181958041c1b7ab3b
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2023-02-27 01:54:58 +0800

    Done partial map function

--- src/components/MapFeed.jsx
+++ src/components/MapFeed.jsx
@@ -1,29 +1,44 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import { BACKEND_URL } from "../constants.js";
 
 const MapFeed = () => {
-  // const [locationData, setLocationData] = useState([]);
+  const [locationData, setLocationData] = useState([]);
   // const [mapURL, setMapURL] = useState("");
+
+  useEffect(() => {
+    axios.get(`${BACKEND_URL}/locations`).then((res) => {
+      setLocationData(res.data);
+    });
+  }, []);
 
   const originZoom = 17;
   const width = 512;
-  const height = 512;
+  const height = 412;
   const mapOrigin = {
-    latitude: 1.3450052275449973,
-    longitude: 103.68234365882233,
+    latitude: 1.3431959893186116,
+    longitude: 103.68110593275408,
   };
-  const locationData = {
-    latitude: 1.343572587250799,
-    longitude: 103.68106886194161,
-    id: 1,
-  };
-  let mapURL = `https://developers.onemap.sg/commonapi/staticmap/getStaticImage?layerchosen=grey&lat=${mapOrigin.latitude}&lng=${mapOrigin.longitude}&zoom=${originZoom}&width=${width}&height=${height}&points=[${locationData.latitude},${locationData.longitude},"175,50,0","${locationData.id}"]`;
+
+  let locationArray;
+  if (locationData) {
+    locationArray = locationData
+      .slice(5)
+      .map(
+        (location) =>
+          `[${location.latitude},${location.longitude},"255,232,31","O"]`,
+      )
+      .join("|");
+  }
+
+  console.log(locationArray)
+
+  let mapURL = `https://developers.onemap.sg/commonapi/staticmap/getStaticImage?layerchosen=grey&lat=${mapOrigin.latitude}&lng=${mapOrigin.longitude}&zoom=${originZoom}&width=${width}&height=${height}&points=${locationArray}`;
 
   return (
     <div>
-      <img src={mapURL} alt="map" className="rounded-xl" />
+      <img src={mapURL} alt="map" className="rounded-xl w-[850px] h-[650px] ml-36" />
     </div>
   );
 };
