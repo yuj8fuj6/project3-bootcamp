6a90ae16320e1bda710bcf1f7941cb89f861d127
Nicklye26 <nicklye26@gmail.com>
2023-02-27 22:23:48 +0800

    recent merge with main

--- src/components/CourseReg.jsx
+++ src/components/CourseReg.jsx
@@ -1,33 +1,40 @@
 import React, { useEffect, useState } from "react";
-import Button from "../components/Button"
+import Button from "../components/Button";
 import axios from "axios";
 import useSWR from "swr";
-import useSWRMutation from 'swr/mutation' 
+import useSWRMutation from "swr/mutation";
 import { BACKEND_URL } from "../constants";
+import { Modal } from "antd";
+import CourseModal from "./CourseModal";
+import "./courseReg.css";
+
 const fetcher = (url) => axios.get(url).then((res) => res.data);
 let courses = "";
 
 const CourseReg = (props) => {
-  const studentData = props.studentData
+  const studentData = props.studentData;
   const [courses, setCourse] = useState(""); //used as a query param
   const { data: indexData } = useGetIndexData(courses);
+  const [courseName, setCourseName] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [courseData, setCourseData] = useState({});
   // add course function: add courses to query to backend and add it to courseIndex to be transferred to other components
   const addCourse = () => {
     let course_code = prompt("Please enter course code", "");
-    if(courses.length === 0){
-      setCourse(course_code)
-    }
-    else{
-      setCourse(courses + `+${course_code}`)
+    if (courses.length === 0) {
+      setCourse(course_code);
+    } else {
+      setCourse(courses + `+${course_code}`);
     }
     let course_index = {
-      ...props.courseIndex
-    }
+      ...props.courseIndex,
+    };
     course_index[course_code] = undefined;
-    props.setCourseIndex(course_index)
-  }
+    props.setCourseIndex(course_index);
+    setCourseName(course_code);
+  };
 
-  const deleteCourse = () =>{
+  const deleteCourse = () => {
     let course_index = {
       ...props.courseIndex,
     };
@@ -35,45 +42,47 @@ const CourseReg = (props) => {
     let course_code = prompt("Please enter course code", "");
     if (!courses.length) {
       alert("There is no courses to delete");
-    }
-    else if(courses.indexOf(course_code) !== -1 || course_code === ''){
+    } else if (courses.indexOf(course_code) !== -1 || course_code === "") {
       delete course_index[course_code];
-      console.log(course_code)
-    }
-    else{
+      console.log(course_code);
+    } else {
       alert(`${course_code} is present in your current courses`);
     }
     props.setCourseIndex(course_index);
     let a = courses.indexOf(course_code);
-    courses.splice(a,1)
-    setCourse(courses.join("+"))
-    console.log(courses)
-  }
+    courses.splice(a, 1);
+    setCourse(courses.join("+"));
+    console.log(courses);
+  };
 
-  const handleChange = (e, course) =>{
-    e.preventDefault()
-    let course_index = {...props.courseIndex}
+  const handleChange = (e, course) => {
+    e.preventDefault();
+    let course_index = { ...props.courseIndex };
     console.log(course_index);
-    course_index[course.course_code] = course.course_indices[e.target.value].index_code;
+    course_index[course.course_code] =
+      course.course_indices[e.target.value].index_code;
     //console.log(course_index);
-    props.setCourseIndex(course_index)
-    console.log(course_index)
-  }
+    props.setCourseIndex(course_index);
+    console.log(course_index);
+  };
 
-  const registerCourse = async(e) => {
-    e.preventDefault()
+  const registerCourse = async (e) => {
+    e.preventDefault();
     let indexes = Object.values(props.courseIndex);
     let courses = Object.keys(props.courseIndex);
 
-    indexes = indexes.map((index,i) => {
-      let course = indexData.find(course => course.course_code === courses[i]);
-      let indice = course.course_indices.find(slot => slot.index_code === index)
-      return indice.id
-    })
-    if(indexes.length === 0){
-      console.log("it is empty")
-    }
-    else{
+    indexes = indexes.map((index, i) => {
+      let course = indexData.find(
+        (course) => course.course_code === courses[i]
+      );
+      let indice = course.course_indices.find(
+        (slot) => slot.index_code === index
+      );
+      return indice.id;
+    });
+    if (indexes.length === 0) {
+      console.log("it is empty");
+    } else {
       const data = await axios
         .post(
           `${BACKEND_URL}/courses/register/${studentData.id}/${studentData.student.id}`,
@@ -84,12 +93,29 @@ const CourseReg = (props) => {
         )
         .catch((err) => console.log(err));
     }
-  }
-  let element
-  if(indexData !== undefined){
+  };
+
+  const handleOpenCourse = async () => {
+    await axios.get(`${BACKEND_URL}/courses/${courseName}`).then((response) => {
+      console.log("OPEN COURSE", response);
+      console.log("OPEN COURSE DATA", response.data);
+      setCourseData(response.data);
+    });
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  let element;
+  if (indexData !== undefined) {
     element = indexData.map((course, i) => {
       //console.log(course.course_indices.find(ele => ele.index_code === 320671).vacancy)
-      let options
+      let options;
       if (course.course_indices !== undefined) {
         options = course.course_indices.map((index, i) => {
           let x = <option value={i}>{index.index_code}</option>;
@@ -99,7 +125,20 @@ const CourseReg = (props) => {
       return (
         <tr>
           <th>{i + 1}</th>
-          <td>{course.course_code}</td>
+          <td onClick={handleOpenCourse} className="courseCode">
+            {course.course_code}
+          </td>
+          <Modal
+            title="Course Details"
+            open={isModalOpen}
+            onOk={handleOk}
+            onCancel={handleCancel}
+            width="30vw"
+            footer={null}
+            className="modalBody"
+          >
+            <CourseModal courseData={courseData} />
+          </Modal>
           <select
             className="select select-bordered w-full max-w-xs"
             onChange={(e) => handleChange(e, course)}
@@ -136,10 +175,13 @@ const CourseReg = (props) => {
       <Button onClick={deleteCourse}>Delete Course</Button>
     </div>
   );
-}
+};
 //<tbody>{element}</tbody>;
-export default CourseReg
+export default CourseReg;
 
 function useGetIndexData(courses) {
-  return useSWR(() => (courses ? `${BACKEND_URL}/courses/${courses}` : null), fetcher);
+  return useSWR(
+    () => (courses ? `${BACKEND_URL}/courses/${courses}` : null),
+    fetcher
+  );
 }
